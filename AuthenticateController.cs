b3cb7a27d8d2f0b75c8ac1555d4d56f7346847f2
maheshDivekar <42762536+maheshDivekar@users.noreply.github.com>
2023-03-21 12:40:33 +0530

    Add files via upload

---
+++ AuthenticateController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ServiceLayer.AbstractServices;
+using DomainLayer;
+
+namespace CleanCoreAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthenticateController : ControllerBase
+    {
+        private readonly IAuthenticate _IAuthenticate;
+        public AuthenticateController(IAuthenticate IAuthenticate)
+        {
+            _IAuthenticate = IAuthenticate;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("Authenticate")]
+        public IActionResult Authenticate([FromBody] Login model)
+        {
+
+            var User = _IAuthenticate.AuthenticateUser(model.Username, model.Password);
+            // throw new Exception("Exception in Login method");
+            if (User == null)
+            {
+                return BadRequest(new { message = "Username or password is incorrect" });
+            }
+            return Ok(User);
+            //else
+            //{
+
+            //    var tokenstring = _iJWTGeneration.GenerateJwtToken(User);
+            //    var refreshtokenstring = _iJWTGeneration.GenerateRefreshToken(ipAddress());
+            //    var response = Ok(new
+            //    {
+            //        token = tokenstring,
+            //        userDetails = User,
+            //        refreshtoken = refreshtokenstring.Token,
+            //    });
+
+            //    setTokenCookie(refreshtokenstring.Token);
+            //    return Ok(response);
+            //}
+
+
+        }
+    }
+}
